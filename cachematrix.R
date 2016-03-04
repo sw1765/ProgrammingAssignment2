82460531828c3b1cdc4c726499915df683014fd4
sw1765 <sw1765@att.com>
2016-03-04 15:06:55 -0800

    Add files via upload
    R Programming - programming assignement 3 - cachematrix.R - 3/4/2016 -

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+##	Programming assignment for Week 3, R Programming Class from Coursera, John Hopkins Univ.
+##      Mar 4, 2016
 
-## Write a short comment describing this function
+##Student:  Steve Willis
+
+##      Note:  Specification says to assume that a square, invertible matrix will 
+##             be passed in as the matrix argument
 
 makeCacheMatrix <- function(x = matrix()) {
 
+##    Create Functions to save local copy of matrix inverse, and  
+##    Use environment to store cached copy of matrix inverse
+
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setInv <- function(inverse) inv <<- inverse
+	getInv <- function() inv
+	list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+cacheSolve <-function(x,...) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##    Check for previous calculation of inverse for this object
+##    If object exists, return it, otherwise cacluate, store in 
+##       env and return inverse to calling function
+
+
+	inv = x$getInv()
+	if (!is.null(inv)) {
+	    message("getting cached Inv Matrix")
+	    return(inv)
+	}
+        mat.data = x$get()
+
+	inv <- solve(mat.data,...)
+	x$setInv(inv)
+	return(inv)
 }
+
